1f184cadb3999c9ca38cdc1cf97689427e6e2ff9
AnzeLovse <tigerpower28@gmail.com>
2018-05-11 06:31:11 +0200

    Almost complete model, unresolved evaluate

--- train.py
+++ train.py
@@ -10,15 +10,13 @@ def load_data(path):
     return data
 
 
-def run():
-    protein = "1_PARCLIP_AGO1234_hg19"
+def get_seq(protein, t_data):
     training_data = load_data("data/clip/%s/5000/training_sample_0"% protein)
     x_train = []
-    y_train = []
+    
     
     for record in SeqIO.parse(training_data,"fasta"):
-        sequence = list(record.seq)
-        #print(sequence)                
+        sequence = list(record.seq)                
         nucleotide = {'A' : 0, 'T' : 1, 'G' : 2, 'C' : 3, 'N' : 4} 
         num_seq = list() #sekvenca v številskem formatu
         
@@ -28,28 +26,38 @@ def run():
 
 
         X = np.zeros((len(num_seq),4))
+
         for i in range (len(num_seq)):
             if num_seq[i] <= 3:
-                X[i,num_seq[i]] = 1
-        #print (X)
-        #print (num_seq)
-        #print (len(record.seq))
-
-        
-        
-        y_train.append([int((record.description).split(":")[1])])
-    	#class dobim tako da opis fasta locim po : in dobim vrednost
-
-
-        
+                X[i,num_seq[i]] = 1               
+               
         x_train.append(X)
+
+
     x_train = np.array(x_train)
-    #print(x_train, y_train)
+    return (x_train)
         
   
+def get_class(protein, t_data):
+    y_train = []
+    
 
+    if t_data == 'train':
+        data = load_data("data/clip/%s/5000/training_sample_0"% protein)
+
+    elif t_data == 'test':
+        data = load_data("data/clip/%s/5000/test_sample_0"% protein)
+
+
+    for record in SeqIO.parse(data,"fasta"):
+        y_train.append([int((record.description).split(":")[1])])
+
+
+    return np.array(y_train)
+    
+def run (protein):
     model = Sequential()
-    model.add(Conv1D(32,kernel_size = 26, input_shape=(101,4), strides = 1, padding='valid', activation='relu'))
+    model.add(Conv1D(100,kernel_size = 26, input_shape=(101,4), strides = 1, padding='valid', activation='relu'))
     
     model.add(MaxPooling1D(pool_size=13, strides=13, padding='valid'))
     
@@ -69,15 +77,10 @@ def run():
 
     model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-    model.fit(np.array(x_train), np.array(y_train), epochs=10, batch_size=32)#so podatki v pravilni obliki?
+    model.fit(get_seq(protein,"train"), get_class(protein,"train"), epochs=10, batch_size=100)
 
 
-    score = model.evaluate(np.array(x_train), np.array(y_train), batch_size=32)
+    score = model.evaluate(get_seq(protein,"train"), get_class(protein,"train"), batch_size=100) #test data je drugacne velikosti samo 1000 primerov
+    
     print (score)
 
-    print(model.predict(x_train)[13])
-    print(model.predict(x_train)[14])
-    print(model.predict(x_train)[15])
-
-
-    
